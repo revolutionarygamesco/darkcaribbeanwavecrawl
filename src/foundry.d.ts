ad07620f2e3ebecfd60eade7d8a226618436ff5c
Jason Godesky <jefgodesky@gmail.com>
2025-09-30 20:24:33 -0400

    Remove fvtt-types
    More trouble than it's worth. Doesn't work with WebStorm at all, and I have to add special permissions for everything I want to use anyway. The whole reason I wanted to use some established types was to get some type safety that I didn't have to continuously fuss over. If I have to fuss over it every time I add something anyway, I might as well roll my own.

---
+++ src/foundry.d.ts
@@ -0,0 +1,51 @@
+interface GameSettings {
+  name: string
+  hint: string
+  scope: string
+  config: boolean
+  type: any
+  default: any
+}
+
+interface Actor {
+  id: string
+}
+
+interface Module {
+  api: Record<string, Function>
+}
+
+interface Scene {
+  tokens: {
+    find: (callback: (t: Token) => boolean) => Token | undefined
+  }
+}
+
+interface Token {
+  actorId: string
+  id: string
+}
+
+declare const Hooks: {
+  once: (name: string, callback: () => void) => void
+}
+
+declare const game: {
+  i18n: {
+    localize: (key: string) => string
+  },
+  actors: {
+    get: (id: string) => Actor | undefined
+  },
+  modules: {
+    get: (id: string) => Module
+  },
+  scenes: {
+    active?: Scene
+  },
+  settings: {
+    register: (namespace: string, name: string, settings: GameSettings) => void,
+    set: <T>(namespace: string, name: string, value: T) => Promise<T>,
+    get: <T>(namespace: string, name: string) => T
+  }
+}
