f38f591083b7293e14b4f50bf6c0ab44b6a05fd8
Jason Godesky <jefgodesky@gmail.com>
2025-10-02 21:40:57 -0400

    getDate: Fix!

--- src/scripts/date/get.ts
+++ src/scripts/date/get.ts
@@ -1,9 +1,11 @@
 import getStartDate from './get-start.ts'
+import getMinutes from './get-minutes.ts'
 
 const getDate = (): Date => {
   const start = getStartDate()
-  const elapsed = game.time.worldTime * 1000
-  return new Date(start.getTime() + elapsed)
+  const minutes = getMinutes()
+  const msPerMinute = 60 * 1000
+  return new Date(start.getTime() + (minutes * msPerMinute))
 }
 
 export default getDate
