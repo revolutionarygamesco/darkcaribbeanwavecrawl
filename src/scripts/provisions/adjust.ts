3a77d89fb645f2727ff1b96d0c77996dcc8cb889
Jason Godesky <jefgodesky@gmail.com>
2025-10-03 13:54:50 -0400

    adjustProvisions: Provisions cannot be adjusted below zero

--- src/scripts/provisions/adjust.ts
+++ src/scripts/provisions/adjust.ts
@@ -4,7 +4,7 @@ import setProvisions from './set.ts'
 
 const adjustProvisions = async (type: ProvisionsType, amount: number): Promise<number> => {
   const before = getProvisions(type)
-  const adjusted = before + amount
+  const adjusted = Math.max(before + amount, 0)
   await setProvisions(type, amount)
   return adjusted
 }
