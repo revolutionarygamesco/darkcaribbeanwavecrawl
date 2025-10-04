dc671a8e5dc64744949790726fa30432645f051d
Jason Godesky <jefgodesky@gmail.com>
2025-10-03 21:09:49 -0400

    calculateDayNight: Improve localization

--- src/scripts/date/day-night.test.ts
+++ src/scripts/date/day-night.test.ts
@@ -1,11 +1,12 @@
+import { MODULE_ID } from '../settings.ts'
 import calculateDayNight from './day-night'
 
 describe('calculateDayNight', () => {
   it.each([
-    ['dawn', '24 July 1715, 6:00 AM'],
-    ['day', '24 July 1715, 3:00 PM'],
-    ['dusk', '24 July 1715, 6:30 PM'],
-    ['night', '24 July 1715, 11:00 PM']
+    [`${MODULE_ID}.time.dawn`, '24 July 1715, 6:00 AM'],
+    [`${MODULE_ID}.time.day`, '24 July 1715, 3:00 PM'],
+    [`${MODULE_ID}.time.dusk`, '24 July 1715, 6:30 PM'],
+    [`${MODULE_ID}.time.night`, '24 July 1715, 11:00 PM']
   ])(`returns %s for %s`, (expected, dateStr) => {
     expect(calculateDayNight(new Date(dateStr))).toBe(expected)
   })
--- src/scripts/date/day-night.ts
+++ src/scripts/date/day-night.ts
@@ -1,12 +1,14 @@
+import { MODULE_ID } from '../settings.ts'
 import calculateSunAngle from './sun.ts'
+import localize from '../utilities/localize.ts'
 
-const calculateDayNight = (date: Date): 'dawn' | 'day' | 'dusk' | 'night' => {
+const calculateDayNight = (date: Date): string => {
   const angle = calculateSunAngle(date)
   const hour = date.getHours()
-  if (angle > 6) return 'day'
-  if (angle > -6 && hour < 12) return 'dawn'
-  if (angle > -6 && hour >= 12) return 'dusk'
-  return 'night'
+  if (angle > 6) return localize(`${MODULE_ID}.time.day`)
+  if (angle > -6 && hour < 12) return localize(`${MODULE_ID}.time.dawn`)
+  if (angle > -6 && hour >= 12) return localize(`${MODULE_ID}.time.dusk`)
+  return localize(`${MODULE_ID}.time.night`)
 }
 
 export default calculateDayNight
