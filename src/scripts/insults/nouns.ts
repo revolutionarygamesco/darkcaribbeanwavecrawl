1a6f42ca56f38d777be2ee43e7895f9ebefb24e2
Jason Godesky <jefgodesky@gmail.com>
2025-10-08 22:32:14 -0400

    Insult generator: 100 nouns

--- src/scripts/insults/nouns.ts
+++ src/scripts/insults/nouns.ts
@@ -1,12 +1,152 @@
-export default [
+// Of the form "son of a..."
+const child = [
+  'son of a bitch',
+  'son of a whore',
+  'son of a biscuit eater',
+  'son of a sea cook',
+  'son of the devil',
+  'son of a motherless goat',
+  'son of a fish-fucker',
+  'son of a farmer',
+  'son of a sea hag',
+  'son of a witch'
+]
+
+// Someone who is not a seaman
+const lubber = [
+  'horse-marine',
+  'sea dandy',
+  'landsman',
+  'shore-hugger',
+  'dock-wolloper',
+  'harbor rat',
+  'farmer',
+  'dirt-lover',
+  'tide-waiter',
+  'wharf rat'
+]
+
+// A seaman
+const sailor = [
+  'swabby',
+  'tar',
+  'jack-tar',
+  'shippy',
+  'steady-sweeper',
+  'sea dog',
+  'old salt',
+  'deck ape',
+  'matelot',
+  'lobscouser'
+]
+
+// Animals
+const animals = [
   'dog',
+  'cur',
+  'whelp',
   'sea bass',
-  'strumpet',
-  'knave',
-  'swab',
-  'bedswerver',
+  'pig',
+  'bilge rat',
+  'goat',
+  'cockroach',
+  'worm',
+  'weevil'
+]
+
+// Unpleasant elements of life at sea
+const barnacle = [
   'barnacle',
+  'sea scum',
   'shark bait',
+  'hardtack',
+  'bilge bucket',
+  'sea slime',
+  'jellyfish',
+  'sea snake',
+  'bottom-feeder',
+  'shipworm'
+]
+
+// An immoral person
+const villain = [
+  'blighter',
+  'blackguard',
+  'scoundrel',
+  'rapscallion',
+  'knave',
+  'villain',
+  'jackanape',
+  'wretch',
+  'blaggard',
+  'picaroon'
+]
+
+// An unintelligent person
+const idiot = [
+  'blighter',
+  'buffoon',
+  'oaf',
+  'dolt',
+  'idler',
+  'jobbernowl',
+  'beef-head',
+  'mopus',
+  'dull-swift',
+  'paper skull'
+]
+
+// A weak person
+const weakling = [
+  'pipsqueak',
+  'guttersnipe',
+  'urchin',
+  'milksop',
+  'poltroon',
+  'misfit',
+  'dastard',
+  'whelp',
+  'caitiff',
+  'weakling'
+]
+
+// An irreligious person
+const heretic = [
+  'blasphemer',
+  'heathen',
+  'pagan',
+  'infidel',
+  'apostate',
+  'idolater',
+  'heretic',
+  'demoniac',
+  'warlock',
+  'necromancer'
+]
+
+// Miscellaneous
+const weirdo = [
   'cackle fruit',
-  'son of a biscuit eater'
+  'cat o’ nine tails',
+  'strumpet',
+  'holystone',
+  'powder monkey',
+  'mutineer',
+  'castaway',
+  'turncoat',
+  'bastard',
+  'man o’ war'
+]
+
+export default [
+  ...child,
+  ...lubber,
+  ...sailor,
+  ...animals,
+  ...barnacle,
+  ...villain,
+  ...idiot,
+  ...weakling,
+  ...heretic,
+  ...weirdo
 ]
