0578efe837df6fbae62bf435b1f5836991054d47
Jason Godesky <jefgodesky@gmail.com>
2025-10-09 21:58:17 -0400

    getSavedCrawlStates

--- src/scripts/settings.ts
+++ src/scripts/settings.ts
@@ -1,5 +1,6 @@
 export const MODULE_ID = 'revolutionary-darkcaribbean'
 export const CRAWL_STATE = 'crawl-state'
+export const SAVE_STATE = 'saved-crawl-states'
 
 export const MODULE_SETTINGS = {
   HISTORICAL: 'historical',
