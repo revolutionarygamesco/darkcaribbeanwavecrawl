61ffc851e51bbf977e89335ea5ccf1770a1288c4
Jason Godesky <jefgodesky@gmail.com>
2025-10-11 00:09:51 -0400

    decrChapter

---
+++ src/scripts/state/chapter/decr.test.ts
@@ -0,0 +1,18 @@
+import initCrawlState from '../init.ts'
+import decrChapter from './decr.ts'
+
+describe('decrChapter', () => {
+  it('goes to the previous chapter', async () => {
+    const before = initCrawlState()
+    before.chapter = 6
+    const after = await decrChapter(before, true)
+    expect(after.chapter).toBe(5)
+  })
+
+  it('won’t set a chapter before 1', async () => {
+    const before = initCrawlState()
+    const after = await decrChapter(initCrawlState(), true)
+    expect(after.chapter).toBe(1)
+    expect(after).not.toBe(before)
+  })
+})
