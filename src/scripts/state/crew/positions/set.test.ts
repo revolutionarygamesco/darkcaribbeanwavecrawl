aa5365ac2a29cfa53278bd6af31d113d85b4da6d
Jason Godesky <jefgodesky@gmail.com>
2025-10-11 13:01:12 -0400

    setAssigned: Handle officers
    If you assign crew to quartermaster or sailing master, then they're in charge of either the starboard crew or the larboard crew. As such, they need to be added to that team's crew, and they cannot be a member of the other team's crew. That also means that you can't be the other team's helm or lookout, and you can't be a quartermaster and a sailing master at the same time.

--- src/scripts/state/crew/positions/set.test.ts
+++ src/scripts/state/crew/positions/set.test.ts
@@ -1,10 +1,21 @@
+import CrawlState, { CrawlTeamSide } from '../../state.ts'
 import initCrawlState from '../../init.ts'
 import setAssigned from './set.ts'
 
 describe('setAssigned', () => {
+  const jack = 'calico-jack'
+  const anne = 'anne-bonny'
+  const mary = 'mary-read'
+
+  const expectNotOnTeam = (state: CrawlState, id: string, team: CrawlTeamSide): void => {
+    expect(state.crew.teams[team].crew).not.toContain(id)
+    expect(state.crew.teams[team].helm).toBeUndefined()
+    expect(state.crew.teams[team].lookout).toBeUndefined()
+  }
+
   it.each([
-    ['captain', 'calico-jack'],
-    ['gunner', ['anne-bonny', 'mary-read']],
+    ['captain', jack],
+    ['gunner', [anne, mary]],
     ['captain', []]
   ] as [string, string | string[]][])('sets %s to %s', async (position, value) => {
     const before = initCrawlState()
@@ -13,5 +24,73 @@ describe('setAssigned', () => {
     expect(after.crew.positions[position]).toHaveLength(arr.length)
     expect(after.crew.positions[position]).toEqual(arr)
     expect(after).not.toBe(before)
+  })
+
+  it('adds new quartermaster to starboard crew', async () => {
+    const before = initCrawlState()
+    before.crew.teams.starboard.officer = 'quartermaster'
+    before.crew.teams.larboard.officer = 'sailing-master'
+    before.crew.teams.larboard.crew = [anne]
+    before.crew.teams.larboard.helm = anne
+    before.crew.teams.larboard.lookout = anne
+
+    const actual = await setAssigned('quartermaster', [anne], before, true)
+    expect(actual.crew.teams.starboard.crew).toContain(anne)
+    expectNotOnTeam(actual, anne, 'larboard')
+  })
+
+  it('adds new quartermaster to larboard crew', async () => {
+    const before = initCrawlState()
+    before.crew.teams.starboard.officer = 'sailing-master'
+    before.crew.teams.larboard.officer = 'quartermaster'
+    before.crew.teams.starboard.crew = [anne]
+    before.crew.teams.starboard.helm = anne
+    before.crew.teams.starboard.lookout = anne
+
+    const actual = await setAssigned('quartermaster', [anne], before, true)
+    expect(actual.crew.teams.larboard.crew).toContain(anne)
+    expectNotOnTeam(actual, anne, 'starboard')
+  })
+
+  it('adds new sailing master to larboard crew', async () => {
+    const before = initCrawlState()
+    before.crew.teams.starboard.officer = 'quartermaster'
+    before.crew.teams.larboard.officer = 'sailing-master'
+    before.crew.teams.starboard.crew = [anne]
+    before.crew.teams.starboard.helm = anne
+    before.crew.teams.starboard.lookout = anne
+
+    const actual = await setAssigned('sailing-master', [anne], before, true)
+    expect(actual.crew.teams.larboard.crew).toContain(anne)
+    expectNotOnTeam(actual, anne, 'starboard')
+  })
+
+  it('adds new sailing master to starboard crew', async () => {
+    const before = initCrawlState()
+    before.crew.teams.starboard.officer = 'sailing-master'
+    before.crew.teams.larboard.officer = 'quartermaster'
+    before.crew.teams.larboard.crew = [anne]
+    before.crew.teams.larboard.helm = anne
+    before.crew.teams.larboard.lookout = anne
+
+    const actual = await setAssigned('sailing-master', [anne], before, true)
+    expect(actual.crew.teams.starboard.crew).toContain(anne)
+    expectNotOnTeam(actual, anne, 'larboard')
+  })
+
+  it('won’t let you be quartermaster and sailing master', async () => {
+    const before = initCrawlState()
+    before.crew.positions.quartermaster = [anne]
+    const actual = await setAssigned('sailing-master', [anne], before, true)
+    expect(actual.crew.positions.quartermaster).not.toContain(anne)
+    expect(actual.crew.positions['sailing-master']).toContain(anne)
+  })
+
+  it('won’t let you be sailing master and quartermaster', async () => {
+    const before = initCrawlState()
+    before.crew.positions['sailing-master'] = [anne]
+    const actual = await setAssigned('quartermaster', [anne], before, true)
+    expect(actual.crew.positions.quartermaster).toContain(anne)
+    expect(actual.crew.positions['sailing-master']).not.toContain(anne)
   })
 })
--- src/scripts/state/crew/positions/set.ts
+++ src/scripts/state/crew/positions/set.ts
@@ -1,7 +1,9 @@
-import type CrawlState from '../../state.ts'
+import CrawlState, { CrawlTeamOfficer } from '../../state.ts'
 import getCrawlState from '../../get.ts'
 import setCrawlState from '../../set.ts'
 import cloneCrawlState from '../../clone.ts'
+import getOppositeSide from '../teams/opposite.ts'
+import getOppositeOfficer from '../teams/officer/opposite.ts'
 
 const setAssigned = async (
   position: string,
@@ -11,6 +13,23 @@ const setAssigned = async (
 ): Promise<CrawlState> => {
   const copy = cloneCrawlState(previous)
   copy.crew.positions[position] = typeof characters === 'string' ? [characters] : characters
+
+  const officers = ['quartermaster', 'sailing-master']
+  if (officers.includes(position)) {
+    const ids = copy.crew.positions[position]
+    const other = getOppositeOfficer(position as CrawlTeamOfficer)
+    copy.crew.positions[other] = (copy.crew.positions[other] ?? [])
+      .filter(id => !ids.includes(id))
+
+    const team = copy.crew.teams.starboard.officer === position ? 'starboard' : 'larboard'
+    const opp = getOppositeSide(team)
+    const { helm, lookout } = copy.crew.teams[opp]
+    if (helm && ids.includes(helm)) delete copy.crew.teams[opp].helm
+    if (lookout && ids.includes(lookout)) delete copy.crew.teams[opp].lookout
+    copy.crew.teams[opp].crew = copy.crew.teams[opp].crew.filter(id => !ids.includes(id))
+    copy.crew.teams[team].crew = [...new Set([...copy.crew.teams[team].crew, ...ids])]
+  }
+
   return skipSave ? copy : await setCrawlState(copy)
 }
 
