ea050abb983c7710206ce19a7c997d8c8b1d5a40
Jason Godesky <jefgodesky@gmail.com>
2025-10-10 21:33:33 -0400

    Remove mapping methods
    Who knows it we'll ever even use them. If we do, it would be better to write a more generic method.

--- src/scripts/state/map-crew-silver.ts
+++
@@ -1,7 +0,0 @@
-const mapCrewSilver = (record: Record<string, number>): Map<string, number> => {
-  const map = new Map<string, number>()
-  for (const key in record) map.set(key, record[key])
-  return map
-}
-
-export default mapCrewSilver
