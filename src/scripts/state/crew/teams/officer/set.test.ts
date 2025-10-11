f24ce83aa3d57887a3d90e842c8aff86f6444f0a
Jason Godesky <jefgodesky@gmail.com>
2025-10-11 13:50:36 -0400

    setOfficer

---
+++ src/scripts/state/crew/teams/officer/set.test.ts
@@ -0,0 +1,27 @@
+import initCrawlState from '../../../init.ts'
+import setOfficer from './set.ts'
+
+describe('setOfficer', () => {
+  const anne = 'anne-bonny'
+  it('sets the team’s officer', async () => {
+    const before = initCrawlState()
+    const after = await setOfficer('starboard', 'quartermaster', before, true)
+    expect(after.crew.teams.starboard.officer).toBe('quartermaster')
+    expect(after.crew.teams.larboard.officer).toBe('sailing-master')
+    expect(after).not.toBe(before)
+  })
+
+  it('moves members to new teams', async () => {
+    const before = initCrawlState()
+    before.crew.positions.quartermaster = [anne]
+    before.crew.teams.larboard.crew = [anne]
+    before.crew.teams.larboard.helm = anne
+    before.crew.teams.larboard.lookout = anne
+
+    const after = await setOfficer('starboard', 'quartermaster', before, true)
+    expect(after.crew.teams.starboard.crew).toContain(anne)
+    expect(after.crew.teams.larboard.crew).not.toContain(anne)
+    expect(after.crew.teams.larboard.helm).toBeUndefined()
+    expect(after.crew.teams.larboard.lookout).toBeUndefined()
+  })
+})
---
+++ src/scripts/state/crew/teams/officer/set.ts
@@ -0,0 +1,40 @@
+import CrawlState, { CrawlTeamSide, CrawlTeamOfficer } from '../../../state.ts'
+import cloneCrawlState from '../../../clone.ts'
+import getOppositeOfficer from './opposite.ts'
+import getOppositeSide from '../opposite.ts'
+import setCrawlState from '../../../set.ts'
+
+const setOfficer = async (
+  side: CrawlTeamSide,
+  officer: CrawlTeamOfficer,
+  previous: CrawlState,
+  skipSave: boolean = false
+): Promise<CrawlState> => {
+  const otherTeam = getOppositeSide(side)
+  const otherOfficer = getOppositeOfficer(officer)
+  const copy = cloneCrawlState(previous)
+
+  copy.crew.teams[side].officer = officer
+  copy.crew.teams[otherTeam].officer = otherOfficer
+
+  let thisCrew = copy.crew.teams[side].crew
+  let thatCrew = copy.crew.teams[otherTeam].crew
+
+  thisCrew = [...new Set([...thisCrew, ...(copy.crew.positions[officer] ?? [])])]
+  thatCrew = [...new Set([...thatCrew, ...(copy.crew.positions[otherOfficer] ?? [])])]
+  thatCrew = thatCrew.filter(id => !thisCrew.includes(id))
+  thisCrew = thisCrew.filter(id => !thatCrew.includes(id))
+
+  const { helm: thisHelm, lookout: thisLookout } = copy.crew.teams[side]
+  const { helm: thatHelm, lookout: thatLookout } = copy.crew.teams[otherTeam]
+  if (thisHelm && !thisCrew.includes(thisHelm)) delete copy.crew.teams[side].helm
+  if (thisLookout && !thisCrew.includes(thisLookout)) delete copy.crew.teams[side].lookout
+  if (thatHelm && !thatCrew.includes(thatHelm)) delete copy.crew.teams[otherTeam].helm
+  if (thatLookout && !thatCrew.includes(thatLookout)) delete copy.crew.teams[otherTeam].lookout
+
+  copy.crew.teams[side].crew = thisCrew
+  copy.crew.teams[otherTeam].crew = thatCrew
+  return skipSave ? copy : await setCrawlState(copy)
+}
+
+export default setOfficer
