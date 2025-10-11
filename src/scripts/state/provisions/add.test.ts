fe68752c1fa1e48f786a03440877c7109654283a
Jason Godesky <jefgodesky@gmail.com>
2025-10-11 09:37:51 -0400

    addProvisions

---
+++ src/scripts/state/provisions/add.test.ts
@@ -0,0 +1,30 @@
+import CrawlState, { CrawlProvisions, Provision } from '../state.ts'
+import initCrawlState from '../init.ts'
+import addProvisions from './add.ts'
+
+describe('addProvisions', () => {
+  let before: CrawlState
+  const starting: CrawlProvisions = { food: 10, water: 20, rum: 5, forage: false }
+
+  beforeAll(() => {
+    before = initCrawlState()
+    before.provisions = starting
+  })
+
+  it.each(['food', 'water', 'rum'] as Provision[])('adds 15 days to the %s stores', async (type: Provision) => {
+    const after = await addProvisions(type, 15, before, true)
+    expect(after.provisions[type]).toBe(starting[type] + 15)
+    expect(before.provisions[type]).toBe(starting[type])
+    expect(after).not.toBe(before)
+  })
+
+  it.each(['food', 'water', 'rum'] as Provision[])('reduces the %s stores by 3 days', async (type: Provision) => {
+    const after = await addProvisions(type, -3, before, true)
+    expect(after.provisions[type]).toBe(starting[type] -3)
+  })
+
+  it.each(['food', 'water', 'rum'] as Provision[])('won’t reduce the %s stores below zero', async (type: Provision) => {
+    const after = await addProvisions(type, -100, before, true)
+    expect(after.provisions[type]).toBe(0)
+  })
+})
---
+++ src/scripts/state/provisions/add.ts
@@ -0,0 +1,15 @@
+import CrawlState, { Provision } from '../state.ts'
+import getCrawlState from '../get.ts'
+import setProvisions from './set.ts'
+
+const addProvisions = async (
+  type: Provision,
+  amount: number,
+  previous: CrawlState = getCrawlState(),
+  skipSave: boolean = false
+): Promise<CrawlState> => {
+  const sum = previous.provisions[type] + amount
+  return await setProvisions(type, sum, previous, skipSave)
+}
+
+export default addProvisions
