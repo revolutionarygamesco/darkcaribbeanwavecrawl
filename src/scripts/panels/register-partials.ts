e2279f69ae42094b32ead0a3c3d2638e228db9e7
Jason Godesky <jefgodesky@gmail.com>
2025-10-13 16:14:17 -0400

    registerPartials

---
+++ src/scripts/panels/register-partials.ts
@@ -0,0 +1,22 @@
+import { MODULE_ID } from '../settings.ts'
+
+const partials = [
+  'listing',
+  'team'
+]
+
+const getFilename = (partial: string): string => {
+  const directory = `modules/${MODULE_ID}/templates`
+  return `${directory}/${partial}.hbs`
+}
+
+const registerPartials = async (): Promise<void> => {
+  const filenames = partials.map(partial => getFilename(partial))
+  await foundry.applications.handlebars.loadTemplates(filenames)
+  await Promise.all(partials.map(async partial => {
+    const template = await foundry.applications.handlebars.getTemplate(getFilename(partial))
+    Handlebars.registerPartial(partial, template)
+  }))
+}
+
+export default registerPartials
