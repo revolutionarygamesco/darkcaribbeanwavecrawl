aae478e3345354bbcc7feb5913d8c29cbdcc51a1
Jason Godesky <jefgodesky@gmail.com>
2025-10-19 13:28:45 -0400

    glossPosition: Use setupCrew for testing

--- src/scripts/panels/helpers/gloss.test.ts
+++ src/scripts/panels/helpers/gloss.test.ts
@@ -1,27 +1,15 @@
 import { MODULE_ID } from '../../settings.ts'
 import { PositionData } from '../../state/state.ts'
+import setupCrew, { jack } from '../../utilities/testing/crew.ts'
 import glossPosition from './gloss.ts'
 
 describe('glossPosition', () => {
-  let originalActors: Map<string, Actor>
-  const jack = 'calico-jack'
+  setupCrew()
+
   const capt: PositionData = {
     assigned: [jack],
     shares: 2
   }
-
-  beforeAll(() => {
-    originalActors = game.actors
-  })
-
-  beforeEach(() => {
-    game.actors = new Map<string, Actor>()
-    game.actors.set(jack, { id: jack } as Actor)
-  })
-
-  afterEach(() => {
-    game.actors = originalActors
-  })
 
   it('glosses the position', () => {
     const actual = glossPosition('captain', capt)
