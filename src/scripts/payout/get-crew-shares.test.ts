2e7422791db9df4ff6e282d8410c913cc88a430f
Jason Godesky <jefgodesky@gmail.com>
2025-10-19 13:41:13 -0400

    getCrewShares: Use setupCrew for testing

--- src/scripts/payout/get-crew-shares.test.ts
+++ src/scripts/payout/get-crew-shares.test.ts
@@ -1,30 +1,13 @@
 import type CrawlState from '../state/state.ts'
-import initCrawlState from '../state/init.ts'
+import setupCrew, { setupState, jack, anne, mary } from '../utilities/testing/crew.ts'
 import getCrewShares from './get-crew-shares.ts'
 
 describe('getCrewShares', () => {
+  setupCrew()
   let state: CrawlState
-  let originalActors: Map<string, Actor>
-  const jack = 'calico-jack'
-  const anne = 'anne-bonny'
-  const mary = 'mary-read'
-
-  beforeAll(() => {
-    originalActors = game.actors
-  })
 
   beforeEach(() => {
-    game.actors = new Map<string, Actor>()
-    for (const id of [jack, anne, mary]) game.actors.set(id, { id } as Actor)
-
-    state = initCrawlState()
-    state.crew.positions.captain = { shares: 2, assigned: [jack] }
-    state.crew.positions.quartermaster = { shares: 1.5, assigned: [anne] }
-    state.crew.positions.gunner = { shares: 1.25, assigned: [anne, mary] }
-  })
-
-  afterEach(() => {
-    game.actors = originalActors
+    state = setupState()
   })
 
   it('returns a table of crew members and shares for each', async () => {
@@ -32,7 +15,7 @@ describe('getCrewShares', () => {
     expect(Object.keys(accounts)).toHaveLength(3)
     expect(accounts[jack].shares).toBe(2)
     expect(accounts[anne].shares).toBe(1.5)
-    expect(accounts[mary].shares).toBe(1.25)
-    expect(total).toBe(2 + 1.5 + 1.25)
+    expect(accounts[mary].shares).toBe(1)
+    expect(total).toBe(2 + 1.5 + 1)
   })
 })
