3923f4e7bb6881de4114b1dbe3251df8f9cb31f9
Jason Godesky <jefgodesky@gmail.com>
2025-10-19 13:38:02 -0400

    mapIdsToActors: Use setupCrew for testing

--- src/scripts/utilities/map-ids-to-actors.test.ts
+++ src/scripts/utilities/map-ids-to-actors.test.ts
@@ -1,21 +1,8 @@
+import setupCrew, { jack } from './testing/crew.ts'
 import mapIdsToActors from './map-ids-to-actors.ts'
 
 describe('mapIdsToActors', () => {
-  let originalActors: Map<string, Actor>
-  const jack = 'calico-jack'
-
-  beforeAll(() => {
-    originalActors = game.actors
-  })
-
-  beforeEach(() => {
-    game.actors = new Map<string, Actor>()
-    game.actors.set(jack, { id: jack } as Actor)
-  })
-
-  afterEach(() => {
-    game.actors = originalActors
-  })
+  setupCrew()
 
   it('maps an array of IDs to an array of Actors', () => {
     const actual = mapIdsToActors([jack, 'nope'])
