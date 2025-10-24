0598058ae6801a6e9104a420458a5a1fca490bae
Jason Godesky <jefgodesky@gmail.com>
2025-10-24 17:33:17 -0400

    assign: Handle corrections

--- src/scripts/state/crew/positions/assign.test.ts
+++ src/scripts/state/crew/positions/assign.test.ts
@@ -1,6 +1,5 @@
 import type CrawlState from '../../state.ts'
-import initCrawlState from '../../init.ts'
-import setupCrew, { jack } from '../../../utilities/testing/crew.ts'
+import setupCrew, { setupState, jack, anne } from '../../../utilities/testing/crew.ts'
 import assign from './assign.ts'
 
 describe('assign', () => {
@@ -9,14 +8,60 @@ describe('assign', () => {
   let before: CrawlState
 
   beforeEach(() => {
-    before = initCrawlState()
-    before.ship.actor = 'william'
+    before = setupState()
   })
 
   it('assigns a character to a position in a new state', async () => {
-    const after = await assign('captain', jack, before, false)
-    const assigned = after?.crew.positions.captain
-    expect(assigned).toEqual([jack])
+    const after = await assign('quartermaster', jack, before, false)
+    expect(after?.crew.positions.quartermaster).toEqual([jack])
     expect(after).not.toBe(before)
+  })
+
+  it('removes character from other positions if new position is exclusive', async () => {
+    const after = await assign('quartermaster', jack, before, false)
+    expect(after?.crew.positions.captain).toHaveLength(0)
+  })
+
+  it('removes character from previously-held exclusive positions', async () => {
+    const after = await assign('crew', jack, before, false)
+    expect(after?.crew.positions.captain).toHaveLength(0)
+    expect(after?.crew.positions.crew).toContain(jack)
+  })
+
+  it('won’t let you exceed the max for a position', async () => {
+    const after = await assign('captain', anne, before, false)
+    expect(after).toBeNull()
+  })
+
+  it('adds you to quartermaster’s watch crew if assigned quartermaster', async () => {
+    const after = await assign('quartermaster', anne, before, false)
+    expect(after?.crew.teams.starboard.members).toContain(anne)
+  })
+
+  it('adds you to sailing master’s watch crew if assigned sailing master', async () => {
+    const after = await assign('master', anne, before, false)
+    expect(after?.crew.teams.larboard.members).toContain(anne)
+  })
+
+  it('removes you from sailing master’s watch crew if assigned quartermaster', async () => {
+    before.crew.teams.larboard.helm = anne
+    before.crew.teams.larboard.lookout = anne
+    before.crew.teams.larboard.members = [anne]
+    const after = await assign('quartermaster', anne, before, false)
+
+    expect(after?.crew.teams.larboard.helm).toBeUndefined()
+    expect(after?.crew.teams.larboard.lookout).toBeUndefined()
+    expect(after?.crew.teams.larboard.members).toHaveLength(0)
+  })
+
+  it('removes you from quartermaster’s watch crew if assigned sailing master', async () => {
+    before.crew.teams.starboard.helm = anne
+    before.crew.teams.starboard.lookout = anne
+    before.crew.teams.starboard.members = [anne]
+    const after = await assign('master', anne, before, false)
+
+    expect(after?.crew.teams.starboard.helm).toBeUndefined()
+    expect(after?.crew.teams.starboard.lookout).toBeUndefined()
+    expect(after?.crew.teams.starboard.members).toHaveLength(0)
   })
 })
--- src/scripts/state/crew/positions/assign.ts
+++ src/scripts/state/crew/positions/assign.ts
@@ -1,7 +1,10 @@
 import type CrawlState from '../../state.ts'
-import getCrawlState from '../../get.ts'
+import getCopy from '../../get-copy.ts'
 import getAssigned from './get.ts'
+import getPositionConfig from '../../crew-config/get-position.ts'
 import setAssigned from './set.ts'
+import getOppositeSide from '../teams/opposite.ts'
+import removeTeamMember from '../teams/remove.ts'
 
 const assign = async (
   position: string,
@@ -9,11 +12,46 @@ const assign = async (
   state?: CrawlState,
   save: boolean = true
 ): Promise<CrawlState | null> => {
-  const previous = state ?? await getCrawlState()
-  const before = (await getAssigned(position, previous)).map(actor => actor.id)
+  const cs = await getCopy(state)
+  let candidate: CrawlState | null = await getCopy(cs)
+  const config = getPositionConfig(position)
+  const max = config?.max
+  const exclusive = config?.exclusive ?? false
+  const otherPositions = Object.keys(cs.crew.positions)
+    .filter(key => key !== position)
+
+  const before = (await getAssigned(position, cs)).map(actor => actor.id)
   const ids = typeof characters === 'string' ? [characters] : characters
   const after = [...new Set([...before, ...ids])]
-  return await setAssigned(position, after, previous, save)
+
+  // Enforce maximum on positions
+  if (max && after.length > max) return null
+
+  // When assigning to exclusive position, resign any other positions
+  if (exclusive) {
+    for (const other of otherPositions) {
+      candidate.crew.positions[other] = candidate.crew.positions[other]
+        .filter(id => !ids.includes(id))
+    }
+  }
+
+  // Resign from any other exclusive position you have
+  const otherExclusive = otherPositions.filter(key => getPositionConfig(key)?.exclusive ?? false)
+  for (const other of otherExclusive) {
+    candidate.crew.positions[other] = candidate.crew.positions[other]
+      .filter(id => !ids.includes(id))
+  }
+
+  // Quartermaster and sailing master each lead a watch crew
+  const officers = ['quartermaster', 'master']
+  if (officers.includes(position)) {
+    const side = candidate.crew.teams.starboard.officer === position ? 'starboard' : 'larboard'
+    const other = getOppositeSide(side)
+    removeTeamMember(other, ids, candidate)
+    candidate.crew.teams[side].members.push(...ids)
+  }
+
+  return await setAssigned(position, after, candidate, save)
 }
 
 export default assign
