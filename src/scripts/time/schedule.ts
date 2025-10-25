4c6401529bf6e87cb79564f63f6eb9a30771510c
Jason Godesky <jefgodesky@gmail.com>
2025-10-25 10:12:38 -0400

    Schedule

---
+++ src/scripts/time/schedule.ts
@@ -0,0 +1,17 @@
+export interface ScheduleItem {
+  hour: number
+  end: string
+  start: string
+}
+
+const schedule: ScheduleItem[] = [
+  { hour: 0, end: 'first', start: 'middle' },
+  { hour: 4, end: 'middle', start: 'morning' },
+  { hour: 8, end: 'morning', start: 'forenoon' },
+  { hour: 12, end: 'forenoon', start: 'afternoon' },
+  { hour: 16, end: 'afternoon', start: 'first dog' },
+  { hour: 18, end: 'first dog', start: 'second dog' },
+  { hour: 20, end: 'second dog', start: 'first' }
+]
+
+export default schedule
