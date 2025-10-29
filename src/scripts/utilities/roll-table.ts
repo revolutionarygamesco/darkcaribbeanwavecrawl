bde014f7953436dde05c642f8ae16d7c6101a111
Jason Godesky <jefgodesky@gmail.com>
2025-10-29 19:13:57 -0400

    rollTable

---
+++ src/scripts/utilities/roll-table.ts
@@ -0,0 +1,24 @@
+import ids from '../ids.ts'
+
+interface RollTableResult {
+  name: string
+  description: string
+}
+
+const rollTable = async (
+  id: keyof typeof ids.tables,
+  options?: any
+): Promise<RollTableResult | null> => {
+  if (!game?.tables) return null
+  const table = game.tables.get(ids.tables[id])
+  if (!table) return null
+
+  const drawn = await table.draw(options)
+  if (!drawn || !drawn.results || !Array.isArray(drawn.results) || drawn.results < 1) return null
+  const name = drawn.results[0].name
+  const description = drawn.results[0].description
+  if (!name || !description) return null
+  return { name, description }
+}
+
+export default rollTable
