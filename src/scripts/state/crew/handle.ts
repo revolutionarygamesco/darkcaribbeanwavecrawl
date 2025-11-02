2decfe91ba869f871d9cad5da063720a0ba584ff
Jason Godesky <jefgodesky@gmail.com>
2025-11-02 10:00:40 -0500

    handleShipUpdate: Also remove

--- src/scripts/state/crew/handle.ts
+++ src/scripts/state/crew/handle.ts
@@ -1,5 +1,6 @@
 import assign from './positions/assign.ts'
-import getCrawlState from '../get.ts'
+import removeFromCrew from './remove.ts'
+import getCopy from '../get-copy.ts'
 import getRosterActors from './roster/actors.ts'
 import setCrawlState from '../set.ts'
 import saveCrawlState from '../save.ts'
@@ -8,7 +9,7 @@ const handleShipUpdate = async (document: Document): Promise<void> => {
   if (document.type !== 'vehicle') return
 
   let save = false
-  let state = await getCrawlState()
+  let state = await getCopy()
   if (state.ship.actor !== document.id) return
 
   if (document.system?.captain && !state.crew.positions.captain.includes(document.system.captain)) {
@@ -19,9 +20,19 @@ const handleShipUpdate = async (document: Document): Promise<void> => {
   const roster = await getRosterActors(state)
   const rosterIds = roster.map(actor => actor.id)
   const shipCrewIds: string[] = document.system?.crews ?? []
+
+  // Anyone listed by the ship should be added as crew to the state
   for (const id of shipCrewIds) {
     if (!rosterIds.includes(id)) {
       state = await assign('crew', id, state, false) ?? state
+      save = true
+    }
+  }
+
+  // Anyone who isn't listed by the ship should be removed as crew from the state
+  for (const id of rosterIds) {
+    if (!shipCrewIds.includes(id)) {
+      state = await removeFromCrew(id, state, false) ?? state
       save = true
     }
   }
